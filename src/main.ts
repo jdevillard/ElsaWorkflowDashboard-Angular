0491d4ec930dae498057ed306b87054a308b0546
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2022-06-03 16:43:02 +0200

    add loading of Elsa WebComponent and replace app.component with Elsa Dashboard

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Note: loader import location set using "esmLoaderPath" within the output target config
+import { defineCustomElements as elsaCustomElements } from '@elsa-workflows/elsa-workflows-studio/loader';
+elsaCustomElements();
+
 if (environment.production) {
   enableProdMode();
 }
