474ed8ebee6679faa96606d1b8ae6bab72dd8b2f
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2023-04-24 23:48:00 +0200

    add url loading from controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('elsaStudioRoot', { static: true }) elsaStudioRootRef!: ElementRef;
+  
   title = 'ElsaWorkflowDashboard-Angularv12';
+
+  url="https://localhost:11000";
+
+  ngAfterViewInit(): void {
+    //Try remove the [serverUrl]="url" to use this initialization Method
+    this.elsaStudioRootRef.nativeElement.setAttribute('server-url', this.url);
+  }
+
 }
